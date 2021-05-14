832dfce9d32695adc4dc83fb458917616b08add9
Alexandre Badin <alexandre.badin33@gmail.com>
2021-05-14 16:14:15 +0200

    feat(list#index) - mise en forme list#index

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -21,6 +21,12 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to '/'
+  end
+
   private
 
  def list_params
